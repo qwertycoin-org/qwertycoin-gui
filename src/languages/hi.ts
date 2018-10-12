2a7fe446c0d2cdd3cca74cfdf41bad891140127d
Jake Smith <6624930+nnian@users.noreply.github.com>
2018-10-12 12:19:46 +0200

    Update language files

--- src/languages/hi.ts
+++ src/languages/hi.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="hi" sourcelanguage="en">
+<TS version="2.0" language="hi">
 <context>
     <name>AboutDialog</name>
     <message>
