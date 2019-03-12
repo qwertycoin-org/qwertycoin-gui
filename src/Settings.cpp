e158da9fb20d66b71e349a246edc87a432707982
Mr QWC <qwertycoin@protonmail.com>
2019-03-12 23:18:29 +0100

    add missing ports on nodesList

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -101,7 +101,7 @@ void Settings::load() {
   }
 
   QStringList defaultNodesList;
-  defaultNodesList << "node-00.qwertycoin.org:8197" << "node-01.qwertycoin.org" << "node-02.qwertycoin.org" << "explorer.qwertycoin.org";
+  defaultNodesList << "node-00.qwertycoin.org:8197" << "node-01.qwertycoin.org:8197" << "node-02.qwertycoin.org:8197"  << "node-03.qwertycoin.org:8197"  << "node-04.qwertycoin.org:8197"  << "node-05.qwertycoin.org:8197" << "explorer.qwertycoin.org:8197";
   if (!m_settings.contains(OPTION_RPCNODES)) {
     setRpcNodesList(QStringList() << defaultNodesList);
   } else {
