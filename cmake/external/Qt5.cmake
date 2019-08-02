14029e025677727d1b7dbe969dda7251c02de0e0
ExploShot <marc.wrba@gmail.com>
2019-08-02 12:51:53 +0200

    Update build automation

--- cmake/external/Qt5.cmake
+++ cmake/external/Qt5.cmake
@@ -6,16 +6,50 @@ set(CMAKE_AUTOUIC OFF) # Create code from a list of Qt designer ui files
 set(CMAKE_AUTORCC ON) # Automatically handle Qt resource files
 
 set(Qt5_COMPONENTS Core Gui Network Widgets)
-if(APPLE)
+if (APPLE)
     list(APPEND Qt5_COMPONENTS PrintSupport)
-endif()
+endif ()
 
-if(EXISTS "$ENV{Qt5_DIR}" AND IS_DIRECTORY "$ENV{Qt5_DIR}")
+if (EXISTS "$ENV{Qt5_DIR}" AND IS_DIRECTORY "$ENV{Qt5_DIR}")
     message(STATUS "Qt5_DIR is set. Using precompiled Qt5.")
     message(STATUS "Qt5_DIR path: $ENV{Qt5_DIR}")
     find_package(Qt5 REQUIRED COMPONENTS ${Qt5_COMPONENTS})
-else()
+else ()
     message(STATUS "Qt5_DIR is not set. Using Hunter (package manager) to install Qt5.")
     hunter_add_package(Qt)
     find_package(Qt5 QUIET REQUIRED COMPONENTS ${Qt5_COMPONENTS})
+endif ()
+
+if(TARGET Qt5::qmake)
+    get_target_property(QT_QMAKE_EXECUTABLE Qt5::qmake IMPORTED_LOCATION)
+    get_filename_component(QT_INSTALL_BINS "${QT_QMAKE_EXECUTABLE}" DIRECTORY)
+    get_filename_component(QT_INSTALL_LIBS "${QT_INSTALL_BINS}/../lib" ABSOLUTE)
+    message(STATUS "QT_QMAKE_EXECUTABLE: ${QT_QMAKE_EXECUTABLE}")
+    message(STATUS "QT_INSTALL_BINS: ${QT_INSTALL_BINS}")
+    message(STATUS "QT_INSTALL_LIBS: ${QT_INSTALL_LIBS}")
+
+    if(APPLE AND EXISTS "${QT_INSTALL_BINS}/macdeployqt")
+        add_executable(Qt5::macdeployqt IMPORTED)
+
+        set_target_properties(
+            Qt5::macdeployqt
+            PROPERTIES IMPORTED_LOCATION "${QT_INSTALL_BINS}/macdeployqt"
+        )
+        set(Qt5_macdeployqt_FOUND TRUE)
+
+        message(STATUS "Found macdeployqt ${QT_INSTALL_BINS}/macdeployqt")
+    elseif(WIN32)
+        add_executable(Qt5::windeployqt IMPORTED)
+
+        set_target_properties(
+            Qt5::windeployqt
+            PROPERTIES IMPORTED_LOCATION "${QT_INSTALL_BINS}/windeployqt.exe"
+        )
+        set(Qt5_windeployqt_FOUND TRUE)
+
+        message(STATUS "Found windeployqt ${QT_INSTALL_BINS}/windeployqt.exe")
+    else()
+        # This is not an error! Android, iOS and Linux don't have this tool.
+        message(STATUS "Qt5 deployment tool (macdeployqt or windeployqt) is not found.")
+    endif()
 endif()
--- cmake/project/CPackConfig.cmake
+++ cmake/project/CPackConfig.cmake
@@ -1,52 +1,53 @@
 set(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
 set(CPACK_PACKAGE_VENDOR "${PROJECT_VENDOR_NAME}")
-set(CPACK_PACKAGE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/dist")
+set(CPACK_PACKAGE_DIRECTORY "${PROJECT_BINARY_DIR}/dist")
 set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
 set(CPACK_PACKAGE_DESCRIPTION "${PROJECT_DESCRIPTION}")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_DESCRIPTION}")
 set(CPACK_PACKAGE_HOMEPAGE_URL "${PROJECT_VENDOR_URL}")
 set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-v${PROJECT_VERSION}-${PROJECT_OS_NAME}")
-set(CPACK_PACKAGE_CONTACT "https://qwertycoin.org")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PROJECT_DISPLAY_NAME}")
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE.txt")
+set(CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/README.md")
 set(CPACK_STRIP_FILES ON)
 
-if(UNIX AND NOT APPLE AND NOT ANDROID) # linux
-    set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/qwertycoin")
+if(PROJECT_OS_LINUX OR PROJECT_OS_POSIX) # Linux
+    message(STATUS "Configuring Linux (or POSIX) package...")
 
-    if(NOT RPMBUILD)
-        set(CPACK_GENERATOR DEB)
-        set(CPACK_SYSTEM_NAME 64-bit)
-        set(CPACK_DEBIAN_PACKAGE_NAME ${CPACK_PACKAGE_NAME})
-        set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR} <dev@qwertycoin.org>")
-        set(CPACK_DEBIAN_PACKAGE_SECTION Office)
-        set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-        set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Qwertycoin QWC wallet
- Qwertycoin is a decentralized, privacy oriented peer-to-peer
- cryptocurrency. It is open-source, nobody owns or controls Qwertycoin
- and everyone can take part.")
-    else()
-        set(CPACK_GENERATOR RPM)
-        set(CPACK_SYSTEM_NAME x86_64)
-        set(CPACK_RPM_PACKAGE_RELEASE ${PROJECT_GIT_COMMIT_ID})
-        set(CPACK_RPM_PACKAGE_LICENSE "MIT")
-        set(CPACK_RPM_PACKAGE_GROUP Office)
-        set(CPACK_RPM_PACKAGE_REQUIRES "qt5-qtbase >= 5.3.2, qt5-qtbase-gui >= 5.3.2")
-        set(CPACK_RPM_PACKAGE_SUMMARY "Qwertycoin QWC wallet")
-        set(CPACK_RPM_PACKAGE_DESCRIPTION "Qwertycoin QWC wallet
- Qwertycoin is a decentralized, privacy oriented peer-to-peer
- cryptocurrency. It is open-source, nobody owns or controls Qwertycoin
- and everyone can take part.")
-    endif ()
-elseif(APPLE) # macos
+    set(CPACK_GENERATOR DEB RPM)
+    set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/qwertycoin-gui")
+
+    # DEB
+    set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_VENDOR} <dev@qwertycoin.org>")
+    set(CPACK_DEBIAN_PACKAGE_SECTION Office)
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS OFF)
+
+    # RPM
+    set(CPACK_RPM_PACKAGE_VENDOR "${CPACK_PACKAGE_VENDOR} <dev@qwertycoin.org>")
+    set(CPACK_RPM_PACKAGE_GROUP Office)
+    set(CPACK_RPM_PACKAGE_LICENSE "MIT")
+    set(CPACK_RPM_COMPRESSION_TYPE "gzip")
+elseif(PROJECT_OS_MACOS) # macOS
+    message(STATUS "Configuring macOS package...")
+
     set(CPACK_GENERATOR DragNDrop)
-    set(MACOSX_BUNDLE_BUNDLE_NAME "Qwertycoin")
-    set(MACOSX_BUNDLE_INFO_STRING "Qwertycoin GUI Wallet")
-    set(MACOSX_BUNDLE_BUNDLE_VERSION "${PROJECT_VERSION}")
-    set(MACOSX_BUNDLE_LONG_VERSION_STRING "${PROJECT_VERSION}")
-    set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}")
-    set(MACOSX_BUNDLE_ICON_FILE qwertycoin.icns)
-elseif(WIN32) # windows
-    set(CPACK_GENERATOR "ZIP")
+    # TODO: Configure additional variables
+    #   - CPACK_DMG_DS_STORE
+    #   - CPACK_DMG_BACKGROUND_IMAGE
+elseif(PROJECT_OS_WINDOWS) # Windows
+    message(STATUS "Configuring Windows package...")
+    set(CPACK_GENERATOR NSIS)
+    set(CPACK_NSIS_DISPLAY_NAME "${PROJECT_DISPLAY_NAME}")
+    set(CPACK_NSIS_PACKAGE_NAME "${PROJECT_DISPLAY_NAME}")
+    set(CPACK_NSIS_HELP_LINK "${PROJECT_VENDOR_URL}")
+    set(CPACK_NSIS_URL_INFO_ABOUT "${PROJECT_VENDOR_URL}")
+    set(CPACK_NSIS_CONTACT "${PROJECT_VENDOR_NAME} <dev@qwertycoin.org>")
+    set(CPACK_NSIS_EXECUTABLES_DIRECTORY "bin")
+    set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${PROJECT_DISPLAY_NAME}.exe")
+else() # unknown
+    message(WARNING "Can't detect platform. Package settings are not initialized!")
 endif()
 
 include(CPack)
